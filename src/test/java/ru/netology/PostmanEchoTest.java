2017411864bc0bc9ac2f3086b167232b63428551
Dima <celischev85@gmail.com>
2026-03-06 21:41:21 +0300

    initil commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,60 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSentData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+
+
+    @Test
+    void shouldReturnDataInUTF8() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Привет мир")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Привет мир"));
+    }
+
+    @Test
+    void shouldReturnComplexUTF8Data() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Тестирование REST API 2024")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Тестирование REST API 2024"));
+    }
+    @Test
+    void shouldFailOnWrongData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Привет мир")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Wrong text"));
+    }
+}
